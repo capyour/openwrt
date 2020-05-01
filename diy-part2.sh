808208c6ee537ecfc4d7c45fcb51ecdb31acf406
capyour <50035153+capyour@users.noreply.github.com>
2020-05-01 21:09:32 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
 # Modify Host Name
-sed -i '/uci commit system/i uci set system.@system[0].hostname=CrazyWrt'  package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i uci set system.@system[0].hostname=OpenWrt'  package/lean/default-settings/files/zzz-default-settings
 
 
 # Update Luci theme argon  
