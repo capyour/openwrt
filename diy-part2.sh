64204c0554f56525b92064a27d4b253845aec773
capyour <50035153+capyour@users.noreply.github.com>
2020-05-03 18:48:05 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,6 @@ sed -i '/uci commit system/i uci set system.@system[0].hostname=OpenWrt'  packag
 
 
 # Update Luci theme argon  
-rm -rf package/lean/luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon  
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
